68c62f56348424f22371a24d9d59cdf2a2c89ee0
Thejana-A <thejanaakmeemana@gmail.com>
2023-06-02 23:40:26 +0530

    June 2

---
+++ lib/feature/splash-screen/onboarding_two.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
+import 'onboarding_three.dart';
+
+class OnboardingTwo extends StatelessWidget {
+
+  Widget build(context){
+
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Stack(
+          children: [
+            Positioned(
+              top: 30, // Set the desired position
+              right: 20,
+              child: RichText(
+                textAlign: TextAlign.right,
+                text: TextSpan(
+                  text: 'Skip',
+                  style: TextStyle( fontSize:15, color: Colors.grey,),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 160, // Set the desired position
+              left: 35,
+              child: Image.asset('image/splash-screen/screen-2.png',
+                  width: 200.0, height: 180.0),
+            ),
+            Positioned(
+                top: 370, // Set the desired position
+                left: 20,
+                child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Color(0xFFE8FCFA),
+                    ),
+                    padding: EdgeInsets.all(16.0),
+                    margin: EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+
+                    child: Column(
+                        children : <Widget>[
+                          RichText(
+                            //textAlign: TextAlign.right,
+                            text: TextSpan(
+                              text: 'Interactive sign language\n \u00A0\u00A0\u00A0\u00A0learning platform',
+                              style: TextStyle( fontWeight: FontWeight.bold, fontSize:18, color: Colors.black, ),
+                            ),
+                          ),
+                          Container(
+                            height:30,
+                          ),
+                          Row(
+                              children: [
+                                RichText(
+                                  text: TextSpan(
+                                    text: '- ',
+                                    style: TextStyle( fontWeight: FontWeight.bold, fontSize:18, color: Color(0xFFE8ACFA), ),
+                                  ),
+                                ),
+                                RichText(
+                                  textAlign: TextAlign.left,
+                                  text: TextSpan(
+                                    text: '- ',
+                                    style: TextStyle( fontWeight: FontWeight.bold, fontSize:18, color: Color(0xFF147B72), ),
+                                  ),
+                                ),
+                                RichText(
+                                  text: TextSpan(
+                                    text: '- ',
+                                    style: TextStyle( fontWeight: FontWeight.bold, fontSize:18, color: Color(0xFFE8ACFA), ),
+                                  ),
+                                ),
+                                Container(
+                                  width:110,
+                                ),
+                                Icon(
+                                  Icons.arrow_circle_right_sharp,
+                                  color: Color(0xFF147B72),
+                                  size: 32.0,
+                                )
+                              ]
+                          )
+                        ]
+
+                    )
+                )
+            ),
+          ],
+        )
+
+    );
+  }
+}
---
+++ lib/feature/splash-screen/splash_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
+import 'onboarding_one.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  /*@override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => OnboardingOne()),
+      );
+    });
+  }*/
+
+  @override
+  Widget build(BuildContext context) {
+    // Hide the status bar and navigation bar for a fullscreen splash screen
+
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: Color(0xFFe2e4e3),
+          // Set the desired background color here
+        ),
+        home: Scaffold(
+          body: Center(
+            child: Material(
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => OnboardingOne()),
+                  );
+                },
+
+                child: Image.asset('image/splash-screen/logo.png',
+                      width: 110.0, height: 110.0),
+
+              ),
+            )
+          )
+        )
+    );
+  }
+
+}
+
+
+
